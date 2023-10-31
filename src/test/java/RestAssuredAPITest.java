a2e504a4223bad3f5ed4b988a3d990a12cac92e7
Sakshi0999 <sakshsethi@deloitte.com>
2023-10-31 12:02:30 +0530

    Initial Commit

---
+++ src/test/java/RestAssuredAPITest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class RestAssuredAPITest {
+
+    @Test
+    public void GetBooksDetails() {
+
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+
+    }
+
+}
